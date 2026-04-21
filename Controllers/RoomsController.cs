f048689b02f566d434c4b05ea848f16eab911b12
ALYCCOMPUTER\48797 <aleks.borowik@gmail.com>
2026-04-21 23:21:33 +0200

    add ASP.NET Core Web API project with RoomsController and in-memory data

---
+++ Controllers/RoomsController.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Mvc;
+using APBD5.Models; 
+
+namespace APBD5.Controllers 
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class RoomsController : ControllerBase
+    {
+        private static List<Room> Rooms = new()
+        {
+            new Room { Id = 1, Name = "A1", BuildingCode = "A", Floor = 1, Capacity = 20, HasProjector = true, IsActive = true },
+            new Room { Id = 2, Name = "B1", BuildingCode = "B", Floor = 2, Capacity = 30, HasProjector = false, IsActive = true },
+            new Room { Id = 3, Name = "C1", BuildingCode = "A", Floor = 3, Capacity = 15, HasProjector = true, IsActive = false }
+        };
+    // GET: api/rooms
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(Rooms);
+    }
+
+    // GET: api/rooms/{id}
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var room = Rooms.FirstOrDefault(r => r.Id == id);
+        if (room == null) return NotFound();
+        return Ok(room);
+    }
+
+    // GET: api/rooms/buinding/{buildlingCode}
+    [HttpGet("building/{buildingCode}")]
+    public IActionResult GetByBuilding(string buildingCode)
+    {
+        var result = Rooms.Where( r => r.BuildingCode == buildingCode).ToList();
+        return Ok(result);
+    }
+
+    // GET: api/rooms??minCapacity=20&hasProjector=true&activeOnly=true
+    [HttpGet("filter")]
+    public IActionResult Filter([FromQuery] int? minCapacity, [FromQuery] bool? hasProjector, [FromQuery] bool? activeOnly)
+    {
+        var result = Rooms.AsQueryable();
+
+        if (minCapacity.HasValue)
+            result = result.Where(r => r.Capacity >= minCapacity);
+
+        if (hasProjector.HasValue)
+            result = result.Where(r => r.HasProjector == hasProjector);
+
+        if (activeOnly == true)
+            result = result.Where(r => r.IsActive);
+
+        return Ok(result);
+    }
+
+    // POST: api/rooms
+    [HttpPost]
+    public IActionResult Create(Room room)
+    {
+        room.Id = Rooms.Max(r => r.Id) + 1;
+        Rooms.Add(room);
+
+        return CreatedAtAction(nameof(GetById), new { id = room.Id}, room);
+    }
+
+    // PUT: api/rooms/{id}
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Room updateRoom)
+    {
+        var room = Rooms.FirstOrDefault(r => r.Id == id);
+        if (room == null) return NotFound(); 
+
+        room.Name = updateRoom.Name;
+        room.BuildingCode = updateRoom.BuildingCode;
+        room.Floor = updateRoom.Floor;
+        room.Capacity = updateRoom.Capacity;
+        room.HasProjector = updateRoom.HasProjector;
+        room.IsActive = updateRoom.IsActive;
+
+        return Ok(room);
+    }
+
+
+    // DELETE: api/rooms/{id}
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var room = Rooms.FirstOrDefault(r => r.Id == id);
+        if (room == null) return NotFound();
+
+        Rooms.Remove(room);
+
+        return NoContent();
+    }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,15 @@
+namespace APBD5.Models
+{
+
+public class Room {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string BuildingCode { get; set; }
+    public int Floor { get; set; }
+    public int Capacity { get; set; }
+    public bool HasProjector { get; set; }
+    public bool IsActive { get; set; }
+}
+
+}
+
